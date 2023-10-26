8652747e6ba3df9e7335e1b26fe53af67e162afd
daniel-baf <68715227+daniel-baf@users.noreply.github.com>
2023-10-26 09:16:37 -0600

    micro

--- docs/0-RES/compiladores/primer-programa.pas
+++ docs/0-RES/compiladores/primer-programa.pas
@@ -2,7 +2,19 @@
 (* lexical analyzer template (TP Lex V3.0), V1.0 3-2-91 AG *)
 
 (* global definitions: *)
+
 Uses lexlib;
+
+Var
+    (*
+        ESTO ES UN COMENTARIO, PODEMOS
+        REALIZAR COMENTARIOS LARGOS CON 
+        LA SINTAXIS (* ... COMENTARIO ... *)
+    *)
+
+	intCounter, decCounter, idCounter  :  integer;  //declara intCounter
+	a1	    :   real;   //declara variable tipo float
+	nombre	:   string;   //declara cadenas
 
 
 
@@ -10,20 +22,30 @@ function yylex : Integer;
 
 procedure yyaction ( yyruleno : Integer );
   (* local definitions: *)
-
 begin
   (* actions: *)
   case yyruleno of
   1:
-      			write('entero');
+                ;
   2:
-          		write('entero');
+          		Begin   // use yytext to extract input
+				    writeLn('entero');
+				    intCounter := intCounter + 1;
+			    end;
   3:
-                     	write('real');
+                    
+                Begin
+                    writeLn('REAL, ingresaste', yytext);
+                    decCounter := decCounter + 1;
+                end;
   4:
-                       	write('identificador');
-
-
+                       	
+                Begin
+                    writeLn('identificador');
+                    idCounter := idCounter + 1;
+                end;
+  5:
+                writeLn('TOKEN INVALIDO');
   end;
 end(*yyaction*);
 
@@ -36,117 +58,220 @@ type YYTRec = record
 
 const
 
-yynmarks   = 4;
-yynmatches = 4;
-yyntrans   = 10;
-yynstates  = 7;
+yynmarks   = 19;
+yynmatches = 19;
+yyntrans   = 38;
+yynstates  = 12;
 
 yyk : array [1..yynmarks] of Integer = (
   { 0: }
+  5,
   { 1: }
+  5,
   { 2: }
   1,
-  2,
   { 3: }
+  2,
+  5,
   { 4: }
+  1,
+  5,
   { 5: }
-  4,
+  5,
   { 6: }
-  3
+  1,
+  5,
+  { 7: }
+  5,
+  { 8: }
+  5,
+  { 9: }
+  1,
+  4,
+  5,
+  { 10: }
+  4,
+  5,
+  { 11: }
+  3,
+  5
 );
 
 yym : array [1..yynmatches] of Integer = (
 { 0: }
+  5,
 { 1: }
+  5,
 { 2: }
   1,
-  2,
 { 3: }
+  2,
+  5,
 { 4: }
+  1,
+  5,
 { 5: }
-  4,
+  5,
 { 6: }
-  3
+  1,
+  5,
+{ 7: }
+  5,
+{ 8: }
+  5,
+{ 9: }
+  1,
+  4,
+  5,
+{ 10: }
+  4,
+  5,
+{ 11: }
+  3,
+  5
 );
 
 yyt : array [1..yyntrans] of YYTrec = (
 { 0: }
-  ( cc: [ '0'..'9' ]; s: 2),
-  ( cc: [ 'a'..'z' ]; s: 3),
+  ( cc: [ #1..#8,#11..'/',':'..'`','{'..#255 ]; s: 7),
+  ( cc: [ #9 ]; s: 6),
+  ( cc: [ #10 ]; s: 2),
+  ( cc: [ '0'..'9' ]; s: 3),
+  ( cc: [ 'a'..'r','t'..'z' ]; s: 5),
+  ( cc: [ 's' ]; s: 4),
 { 1: }
-  ( cc: [ '0'..'9' ]; s: 2),
-  ( cc: [ 'a'..'z' ]; s: 3),
+  ( cc: [ #1..#8,#11..'/',':'..'`','{'..#255 ]; s: 7),
+  ( cc: [ #9 ]; s: 6),
+  ( cc: [ #10 ]; s: 2),
+  ( cc: [ '0'..'9' ]; s: 3),
+  ( cc: [ 'a'..'r','t'..'z' ]; s: 5),
+  ( cc: [ 's' ]; s: 4),
 { 2: }
-  ( cc: [ '.' ]; s: 4),
-  ( cc: [ '0'..'9' ]; s: 2),
+  ( cc: [ #9,#10,'s' ]; s: 2),
 { 3: }
-  ( cc: [ '0'..'9','a'..'z' ]; s: 5),
+  ( cc: [ #1..#9,#11..'-','/',':'..#255 ]; s: 7),
+  ( cc: [ '.' ]; s: 8),
+  ( cc: [ '0'..'9' ]; s: 3),
 { 4: }
-  ( cc: [ '0'..'9' ]; s: 6),
+  ( cc: [ #1..#8,#11..'/',':'..'`','{'..#255 ]; s: 7),
+  ( cc: [ #9 ]; s: 6),
+  ( cc: [ #10 ]; s: 2),
+  ( cc: [ '0'..'9','a'..'r','t'..'z' ]; s: 10),
+  ( cc: [ 's' ]; s: 9),
 { 5: }
-  ( cc: [ '0'..'9','a'..'z' ]; s: 5),
+  ( cc: [ #1..#9,#11..'/',':'..'`','{'..#255 ]; s: 7),
+  ( cc: [ '0'..'9','a'..'z' ]; s: 10),
 { 6: }
-  ( cc: [ '0'..'9' ]; s: 6)
+  ( cc: [ #1..#8,#11..'r','t'..#255 ]; s: 7),
+  ( cc: [ #9,'s' ]; s: 6),
+  ( cc: [ #10 ]; s: 2),
+{ 7: }
+  ( cc: [ #1..#9,#11..#255 ]; s: 7),
+{ 8: }
+  ( cc: [ #1..#9,#11..'/',':'..#255 ]; s: 7),
+  ( cc: [ '0'..'9' ]; s: 11),
+{ 9: }
+  ( cc: [ #1..#8,#11..'/',':'..'`','{'..#255 ]; s: 7),
+  ( cc: [ #9 ]; s: 6),
+  ( cc: [ #10 ]; s: 2),
+  ( cc: [ '0'..'9','a'..'r','t'..'z' ]; s: 10),
+  ( cc: [ 's' ]; s: 9),
+{ 10: }
+  ( cc: [ #1..#9,#11..'/',':'..'`','{'..#255 ]; s: 7),
+  ( cc: [ '0'..'9','a'..'z' ]; s: 10),
+{ 11: }
+  ( cc: [ #1..#9,#11..'/',':'..#255 ]; s: 7),
+  ( cc: [ '0'..'9' ]; s: 11)
 );
 
 yykl : array [0..yynstates-1] of Integer = (
 { 0: } 1,
-{ 1: } 1,
-{ 2: } 1,
-{ 3: } 3,
-{ 4: } 3,
-{ 5: } 3,
-{ 6: } 4
+{ 1: } 2,
+{ 2: } 3,
+{ 3: } 4,
+{ 4: } 6,
+{ 5: } 8,
+{ 6: } 9,
+{ 7: } 11,
+{ 8: } 12,
+{ 9: } 13,
+{ 10: } 16,
+{ 11: } 18
 );
 
 yykh : array [0..yynstates-1] of Integer = (
-{ 0: } 0,
-{ 1: } 0,
-{ 2: } 2,
-{ 3: } 2,
-{ 4: } 2,
-{ 5: } 3,
-{ 6: } 4
+{ 0: } 1,
+{ 1: } 2,
+{ 2: } 3,
+{ 3: } 5,
+{ 4: } 7,
+{ 5: } 8,
+{ 6: } 10,
+{ 7: } 11,
+{ 8: } 12,
+{ 9: } 15,
+{ 10: } 17,
+{ 11: } 19
 );
 
 yyml : array [0..yynstates-1] of Integer = (
 { 0: } 1,
-{ 1: } 1,
-{ 2: } 1,
-{ 3: } 3,
-{ 4: } 3,
-{ 5: } 3,
-{ 6: } 4
+{ 1: } 2,
+{ 2: } 3,
+{ 3: } 4,
+{ 4: } 6,
+{ 5: } 8,
+{ 6: } 9,
+{ 7: } 11,
+{ 8: } 12,
+{ 9: } 13,
+{ 10: } 16,
+{ 11: } 18
 );
 
 yymh : array [0..yynstates-1] of Integer = (
-{ 0: } 0,
-{ 1: } 0,
-{ 2: } 2,
-{ 3: } 2,
-{ 4: } 2,
-{ 5: } 3,
-{ 6: } 4
+{ 0: } 1,
+{ 1: } 2,
+{ 2: } 3,
+{ 3: } 5,
+{ 4: } 7,
+{ 5: } 8,
+{ 6: } 10,
+{ 7: } 11,
+{ 8: } 12,
+{ 9: } 15,
+{ 10: } 17,
+{ 11: } 19
 );
 
 yytl : array [0..yynstates-1] of Integer = (
 { 0: } 1,
-{ 1: } 3,
-{ 2: } 5,
-{ 3: } 7,
-{ 4: } 8,
-{ 5: } 9,
-{ 6: } 10
+{ 1: } 7,
+{ 2: } 13,
+{ 3: } 14,
+{ 4: } 17,
+{ 5: } 22,
+{ 6: } 24,
+{ 7: } 27,
+{ 8: } 28,
+{ 9: } 30,
+{ 10: } 35,
+{ 11: } 37
 );
 
 yyth : array [0..yynstates-1] of Integer = (
-{ 0: } 2,
-{ 1: } 4,
-{ 2: } 6,
-{ 3: } 7,
-{ 4: } 8,
-{ 5: } 9,
-{ 6: } 10
+{ 0: } 6,
+{ 1: } 12,
+{ 2: } 13,
+{ 3: } 16,
+{ 4: } 21,
+{ 5: } 23,
+{ 6: } 26,
+{ 7: } 27,
+{ 8: } 29,
+{ 9: } 34,
+{ 10: } 36,
+{ 11: } 38
 );
 
 
@@ -210,6 +335,13 @@ action:
 end(*yylex*);
 
 
-begin
-yylex()=0
-end.
+Begin
+	
+	If yylex()=0 then;
+    
+    writeLn('------------------------ RESUMEN ------------------------');
+    writeLn('ENTEROS        : ', intCounter);
+    writeLn('DECIMALES      : ', decCounter);
+    writeLn('IDENTIFICADORES: ', idCounter);
+
+End.
---
+++ docs/Apuntes/6-ciclo/microprogramacion/2023-10-26-Segunda-hoja-trabajo.md
@@ -0,0 +1,56 @@
+
+# Ejercicio 1
+
+ARCHIVO: EJER21
+
+Un año bisiesto es aquel año que es divisible entre 4, pero no divisible entre 100, a menos que sea divisible entre 400. Programa guardar en `resul` 1 si es bisiesto, 0 si no es bisiesto.
+
+Usar valores para las pruebas: 
+- 2023d = 0
+- 2024d = 1
+- 1800d = 0 
+- 2000d = 1
+
+# Ejercicio 2
+
+ARCHIVO: EJER22
+
+Un programa que divida una variable de tipo palabra doble entre una variable tipo palabra. Utilice el algoritmo de restas repetidas. El dividendo estará en la variable `dividendo` y el divisor en la variable `divisor`, el cociente y el residuo en las variables de tipo palabra cociente y residuo. 
+
+```
+75/7
+
+->
+75 - 7 = 68  | 1
+68 - 7 = 61  | 2
+61 - 7 = 54  | 3
+54 - 7 = 47  | 4
+47 - 7 = 40  | 5
+40 - 7 = 33  | 6
+33 - 7 = 26  | 7
+26 - 7 = 19  | 8
+19 - 7 = 12  | 9
+12 - 7 = 5   | 10
+5            | residuo
+```
+
+Probar con los valores
+- FFFF FFFF / \<value\>
+- FFFF / \<value\>
+- F / \<value\>
+
+# Ejercicio 3
+
+Programa que calcule el enésimo número de Fibonacci
+
+Usar `JCXZ` recomendado por el ingeniero.
+
+```
+0 1
+1 1
+1 2
+3 2
+3 5
+8 5
+
+```
