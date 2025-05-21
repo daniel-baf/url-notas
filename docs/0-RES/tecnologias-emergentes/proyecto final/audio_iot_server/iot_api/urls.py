2080238b77ffd9a0231b0571ca337349c06a612c
Daniel Bautista <danibaufu@gmail.com>
2025-05-21 10:36:00 -0600

    pre dashboard

--- docs/0-RES/tecnologias-emergentes/proyecto final/audio_iot_server/iot_api/urls.py
+++ docs/0-RES/tecnologias-emergentes/proyecto final/audio_iot_server/iot_api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("upload/", views.upload_audio, name="upload_audio"),
+    path("dashboard/", views.dashboard, name="dashboard"),
 ]
--- docs/0-RES/tecnologias-emergentes/proyecto final/audio_iot_server/iot_api/views.py
+++ docs/0-RES/tecnologias-emergentes/proyecto final/audio_iot_server/iot_api/views.py
@@ -1,11 +1,30 @@
+import os
+import json
+
+from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, JsonResponse
-import os
 from django.conf import settings
-
-from .model.audio_predict import predict_audio
+from collections import deque  # Para manejar las últimas 20 predicciones
 
 from .services.audio_handler import enhance_and_handle_audio
+
+class_mapping_path = os.path.join(
+    os.path.dirname(__file__), "model", "class_mapping.json"
+)
+with open(class_mapping_path, "r") as f:
+    notification_settings = json.load(f)
+
+# Variable global para almacenar las últimas 20 predicciones
+recent_predictions = deque(maxlen=20)
+
+
+def dashboard(request):
+    context = {
+        "recent_predictions": list(recent_predictions),
+        "notification_settings": notification_settings,
+    }
+    return render(request, "dashboard.html", context)
 
 
 @csrf_exempt
@@ -23,7 +42,11 @@ def upload_audio(request):
             with open(save_path, "wb") as f:
                 f.write(request.body)
 
-            enhance_and_handle_audio(save_path)
+            # Obtener predicción
+            prediction = enhance_and_handle_audio(save_path)
+
+            # Guardar predicción en la cola
+            recent_predictions.append(prediction)
 
             return JsonResponse(
                 {"status": "success", "message": "Audio received and saved."},
